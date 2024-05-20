c9c0aa97fb6495879a4d376173153e7dc047650e
abdulsamikhan456 <abdsamikhan456@gmail.com>
2024-05-20 15:21:11 +0500

    Initial commit

--- screens/InventoryManagement.js
+++ screens/InventoryManagement.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, FlatList, TouchableOpacity, Modal, TextInput, Button } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation, useRoute } from '@react-navigation/native';
+import { getFirestore, collection, doc, setDoc, getDocs, query, where ,deleteDoc} from 'firebase/firestore';
 
 const initialInventoryData = [
   {
@@ -26,11 +27,11 @@ const InventoryManagement = () => {
   const navigation = useNavigation();
   const route = useRoute();
   
-    const { userEmail } = route.params;
+  const { userEmail } = route.params;
 
-    console.log("User Email:", userEmail);
+  console.log("User Email:", userEmail);
 
-  const [inventoryData, setInventoryData] = useState(route.params?.inventoryData || initialInventoryData);
+  const [inventoryData, setInventoryData] = useState([]);
   const [modalVisible, setModalVisible] = useState(false);
   const [newItemData, setNewItemData] = useState({
     name: '',
@@ -49,36 +50,72 @@ const InventoryManagement = () => {
   });
 
   useEffect(() => {
-    if (route.params?.newItem) {
-      const newItem = route.params.newItem;
-      setInventoryData(prevData => [...prevData, newItem]);
-    } else if (route.params?.updatedItem) {
-      const updatedItem = route.params.updatedItem;
-      setInventoryData(prevData =>
-        prevData.map(item => (item.id === updatedItem.id ? updatedItem : item))
-      );
+    fetchInventoryData();
+  }, []);
+
+  const fetchInventoryData = async () => {
+    try {
+      const db = getFirestore();
+      const usersCollectionRef = collection(db, 'users');
+      const q = query(usersCollectionRef, where("email", "==", userEmail));
+      const querySnapshot = await getDocs(q);
+
+      if (!querySnapshot.empty) {
+        querySnapshot.forEach(async (userDoc) => {
+          const userDocRef = doc(db, 'users', userDoc.id);
+          const inventoryCollectionRef = collection(userDocRef, 'InventoryManagement');
+          const inventorySnapshot = await getDocs(inventoryCollectionRef);
+          const inventoryData = inventorySnapshot.docs.map(doc => doc.data());
+          setInventoryData([...initialInventoryData, ...inventoryData]); // Merge dummy data with fetched data
+        });
+      } else {
+        console.error("No user found with the provided email!");
+      }
+    } catch (error) {
+      console.error("Error fetching inventory data: ", error);
     }
-  }, [route.params]);
+  };
 
   const toggleModal = () => {
     setModalVisible(!modalVisible);
   };
 
-  const handleAddItem = () => {
+  const handleAddItem = async () => {
     const newItem = {
       id: String(Date.now()), // Generate unique ID
       ...newItemData,
+      quantity: parseInt(newItemData.quantity, 10),
+      price: parseFloat(newItemData.price),
     };
-    setInventoryData(prevData => [...prevData, newItem]);
-    setModalVisible(false);
-    // Reset newItemData for next addition
-    setNewItemData({
-      name: '',
-      quantity: '',
-      price: '',
-      description: '',
-      icon: 'cube-outline',
-    });
+
+    try {
+      const db = getFirestore();
+      const usersCollectionRef = collection(db, 'users');
+      const q = query(usersCollectionRef, where("email", "==", userEmail));
+      const querySnapshot = await getDocs(q);
+
+      if (!querySnapshot.empty) {
+        querySnapshot.forEach(async (userDoc) => {
+          const userDocRef = doc(db, 'users', userDoc.id);
+          const inventoryCollectionRef = collection(userDocRef, 'InventoryManagement');
+          await setDoc(doc(inventoryCollectionRef, newItem.id), newItem);
+          setModalVisible(false);
+          setNewItemData({
+            name: '',
+            quantity: '',
+            price: '',
+            description: '',
+            icon: 'cube-outline',
+          });
+          // Fetch updated inventory data after adding the new item
+          fetchInventoryData();
+        });
+      } else {
+        console.error("No user found with the provided email!");
+      }
+    } catch (error) {
+      console.error("Error adding document: ", error);
+    }
   };
 
   const handleEditItem = () => {
@@ -105,13 +142,39 @@ const InventoryManagement = () => {
     });
   };
 
+  const handleDeleteItem = async (itemId) => {
+    try {
+      const db = getFirestore();
+      const usersCollectionRef = collection(db, 'users');
+      const q = query(usersCollectionRef, where("email", "==", userEmail));
+      const querySnapshot = await getDocs(q);
+
+      if (!querySnapshot.empty) {
+        querySnapshot.forEach(async (userDoc) => {
+          const userDocRef = doc(db, 'users', userDoc.id);
+          const inventoryCollectionRef = collection(userDocRef, 'InventoryManagement');
+          await deleteDoc(doc(inventoryCollectionRef, itemId));
+
+          // Remove the deleted item from inventoryData state
+          setInventoryData(prevData =>
+            prevData.filter(item => item.id !== itemId)
+          );
+        });
+      } else {
+        console.error("No user found with the provided email!");
+      }
+    } catch (error) {
+      console.error("Error deleting document: ", error);
+    }
+  };
+
   const renderItem = ({ item }) => (
     <View style={styles.itemContainer}>
       <Ionicons name={item.icon} size={50} color="black" style={styles.itemIcon} />
       <View style={styles.itemDetails}>
         <Text style={styles.itemName}>{item.name}</Text>
         <Text style={styles.itemQuantity}>Quantity: {item.quantity}</Text>
-        <Text style={styles.itemPrice}>Price:Rs {item.price}</Text>
+        <Text style={styles.itemPrice}>Price: Rs {item.price}</Text>
       </View>
       <TouchableOpacity
         onPress={() => {
@@ -121,7 +184,7 @@ const InventoryManagement = () => {
       >
         <Ionicons name="pencil" size={24} color="black" style={styles.icon} />
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => {/* Handle delete action */}}>
+      <TouchableOpacity onPress={() => handleDeleteItem(item.id)}>
         <Ionicons name="trash" size={24} color="black" style={styles.icon} />
       </TouchableOpacity>
     </View>
@@ -145,7 +208,7 @@ const InventoryManagement = () => {
         style={styles.addButton}
         onPress={toggleModal} // Open modal on button press
       >
-        <Ionicons name="add" size={30} color="white" style={{right: 12}}/>
+        <Ionicons name="add" size={30} color="white" style={{ right: 12 }} />
         <Text style={styles.addButtonText}>Add Item</Text>
       </TouchableOpacity>
       <Modal
@@ -163,27 +226,27 @@ const InventoryManagement = () => {
                   style={styles.input}
                   placeholder="Name"
                   value={editItemData.name}
-                  onChangeText={text => setEditItemData({...editItemData, name: text})}
+                  onChangeText={text => setEditItemData({ ...editItemData, name: text })}
                 />
                 <TextInput
                   style={styles.input}
                   placeholder="Quantity"
                   value={String(editItemData.quantity)} // Convert quantity to string
-                  onChangeText={text => setEditItemData({...editItemData, quantity: text})}
+                  onChangeText={text => setEditItemData({ ...editItemData, quantity: text })}
                   keyboardType="numeric"
                 />
                 <TextInput
                   style={styles.input}
                   placeholder="Price"
                   value={String(editItemData.price)} // Convert price to string
-                  onChangeText={text => setEditItemData({...editItemData, price: text})}
+                  onChangeText={text => setEditItemData({ ...editItemData, price: text })}
                   keyboardType="numeric"
                 />
                 <TextInput
                   style={styles.input}
                   placeholder="Description"
                   value={editItemData.description}
-                  onChangeText={text => setEditItemData({...editItemData, description: text})}
+                  onChangeText={text => setEditItemData({ ...editItemData, description: text })}
                 />
                 <Button title="Save" onPress={handleEditItem} />
               </>
@@ -194,137 +257,134 @@ const InventoryManagement = () => {
                   style={styles.input}
                   placeholder="Name"
                   value={newItemData.name}
-                  onChangeText={text => setNewItemData({...newItemData, name: text})}
+                  onChangeText={text => setNewItemData({ ...newItemData, name: text })}
                 />
                 <TextInput
                   style={styles.input}
                   placeholder="Quantity"
                   value={newItemData.quantity}
-                  onChangeText={text => setNewItemData({...newItemData, quantity: text})}
+                  onChangeText={text => setNewItemData({ ...newItemData, quantity: text })}
                   keyboardType="numeric"
                 />
                 <TextInput
                   style={styles.input}
                   placeholder="Price"
                   value={newItemData.price}
-                  onChangeText={text => setNewItemData({...newItemData, price: text})}
+                  onChangeText={text => setNewItemData({ ...newItemData, price: text })}
                   keyboardType="numeric"
                 />
                 <TextInput
                   style={styles.input}
                   placeholder="Description"
                   value={newItemData.description}
-                  onChangeText={text => setNewItemData
-                    ({...newItemData, description: text})}
-                    /><Button title="Add Now" onPress={handleAddItem} />
-                    </>
-                  )}
-                </View>
-              </View>
-            </Modal>
+                  onChangeText={text => setNewItemData({ ...newItemData, description: text })}
+                />
+                <Button title="Add Now" onPress={handleAddItem} />
+              </>
+            )}
           </View>
-        );
-      };
-      
-      export default InventoryManagement;
-      
-      const styles = StyleSheet.create({
-        container: {
-          flex: 1,
-          backgroundColor: '#fff',
-        },
-        header: {
-          flexDirection: 'row',
-          alignItems: 'center',
-          backgroundColor: '#f9f9f9',
-          paddingVertical: 20,
-          paddingHorizontal: 16,
-          borderBottomWidth: 1,
-          borderBottomColor: '#ddd',
-          top: 10,
-        },
-        headerText: {
-          fontSize: 18,
-          fontWeight: 'bold',
-          marginLeft: 16,
-          left: 55
-        },
-        list: {
-          flexGrow: 1,
-        },
-        itemContainer: {
-          flexDirection: 'row',
-          alignItems: 'center',
-          padding: 16,
-          marginVertical: 12,
-          marginHorizontal: 8,
-          borderWidth: 3,
-          borderColor: '#059fa5',
-          borderRadius: 15,
-          backgroundColor: '#f9f9f9',
-        },
-        itemIcon: {
-          marginRight: 16,
-        },
-        itemDetails: {
-          flex: 1,
-        },
-        itemName: {
-          fontSize: 18,
-          fontWeight: 'bold',
-        },
-        itemQuantity: {
-          fontSize: 14,
-          color: '#888',
-        },
-        itemPrice: {
-          fontSize: 14,
-          color: '#888',
-        },
-        icon: {
-          marginHorizontal: 8,
-        },
-        addButton: {
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'center',
-          backgroundColor: '#059fa5',
-          padding: 25,
-          borderRadius: 30,
-          marginTop: 12,
-          bottom: 5
-        },
-        addButtonText: {
-          color: '#fff',
-          fontSize: 20 ,
-          fontWeight: 'bold',
-          marginLeft: 8,
-          right: 12
-        },
-        modalContainer: {
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: 'rgba(0, 0, 0, 0.5)',
-           // Semi-transparent black background
-        
-        },
-        modalContent: {
-          backgroundColor: '#fff',
-          padding: 20,
-          borderRadius: 10,
-          elevation: 5, // Android elevation
-        },
-        modalTitle: {
-          fontSize: 20,
-          fontWeight: 'bold',
-          marginBottom: 10,
-        },
-        input: {
-          height: 40,
-          borderColor: 'gray',
-          borderWidth: 1,
-          paddingHorizontal: 8,
-          marginBottom: 10,
-        },
-      });
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+export default InventoryManagement;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#f9f9f9',
+    paddingVertical: 20,
+    paddingHorizontal: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+    top: 10,
+  },
+  headerText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginLeft: 16,
+    left: 55
+  },
+  list: {
+    flexGrow: 1,
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 16,
+    marginVertical: 12,
+    marginHorizontal: 8,
+    borderWidth: 3,
+    borderColor: '#059fa5',
+    borderRadius: 15,
+    backgroundColor: '#f9f9f9', },
+    itemIcon: {
+      marginRight: 16,
+    },
+    itemDetails: {
+      flex: 1,
+    },
+    itemName: {
+      fontSize: 18,
+      fontWeight: 'bold',
+    },
+    itemQuantity: {
+      fontSize: 14,
+      color: '#888',
+    },
+    itemPrice: {
+      fontSize: 14,
+      color: '#888',
+    },
+    icon: {
+      marginHorizontal: 8,
+    },
+    addButton: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#059fa5',
+      padding: 25,
+      borderRadius: 30,
+      marginTop: 12,
+      bottom: 5
+    },
+    addButtonText: {
+      color: '#fff',
+      fontSize: 20,
+      fontWeight: 'bold',
+      marginLeft: 8,
+      right: 12
+    },
+    modalContainer: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent black background
+    },
+    modalContent: {
+      backgroundColor: '#fff',
+      padding: 20,
+      borderRadius: 10,
+      elevation: 5, // Android elevation
+    },
+    modalTitle: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      marginBottom: 10,
+    },
+    input: {
+      height: 40,
+      borderColor: 'gray',
+      borderWidth: 1,
+      paddingHorizontal: 8,
+      marginBottom: 10,
+    },
+  });
